9922c388d0e9ef1a1557cbc94fc2003cadae1f22
Marco Boneberger <marco.boneberger@franka.de>
2022-03-28 11:13:09 +0200

    Rebase from 'debian/melodic/libfranka'

--- common/include/research_interface/robot/service_types.h
+++ common/include/research_interface/robot/service_types.h
@@ -14,7 +14,7 @@ namespace robot {
 
 using Version = uint16_t;
 
-constexpr Version kVersion = 4;
+constexpr Version kVersion = 5;
 constexpr uint16_t kCommandPort = 1337;
 
 enum class Command : uint32_t {
